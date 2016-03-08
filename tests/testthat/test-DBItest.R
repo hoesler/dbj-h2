09ef7a7ab3d7bc7297d38534267100a49ce0ef14
Christoph Hösler <christoph.hoesler@gmail.com>
2016-03-08 17:26:49 +0100

    Fixed tests

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,20 +1,20 @@
 DBItest::make_context(
 	# DBItest isn't quoting identifiers with dbQuoteIdentifier().
 	# So we have to use experimantal feature DATABASE_TO_UPPER=FALSE instead.
-	H2(), list(url = "jdbc:h2:mem:dbi-test;DATABASE_TO_UPPER=FALSE", user = 'sa')
+	H2(), list(url = "mem:dbi-test;DATABASE_TO_UPPER=FALSE", user = 'sa')
 )
 
-DBItest::test_getting_started(ctx = ctx, skip = c(
+DBItest::test_getting_started(skip = c(
 	"package_name", # too restrictive
 	"package_dependencies" # 'methods' must be listed in 'Depends' for R <= 3.1.1
 ))
 DBItest::test_driver(skip = c(
 	"constructor_strict", "constructor", # too restrictive
 	"stress_load_unload" # substitute in make_context does not substitute (getOption("h2_jar") is passed on unevaluated)
-), ctx = ctx)
+))
 DBItest::test_connection(skip = c(
 	"stress_load_connect_unload" # substitute in make_context does not substitute (getOption("h2_jar") is passed on unevaluated)
-), ctx = ctx)
+))
 DBItest::test_result(skip = c(
 	"get_query_empty_single_column", "get_query_empty_multi_column", # default implementation of dbGetQuery in 0.3.1 returns NULL for empty results
 	"stale_result_warning", # Currently I have no simple soulution how to keep track of results
@@ -26,7 +26,7 @@ DBItest::test_result(skip = c(
 	"data_raw($|_.+)", # SELECT cast(1 as BLOB) is invalid sytax in H2
 	"data_64_bit($|_.+)", # don't understand the rational behind the test
 	"data_type_connection" # What should be the data type for structure(.(value), class = "unknown1")?
-), ctx = ctx)
+))
 DBItest::test_sql(skip = c(
 	"quote_string", "quote_identifier_not_vectorized", # tests default implementation in DBI 0.3.1.
 	"append_table_error", # append to nonexisting table should only fail if create = FALSE
@@ -37,13 +37,13 @@ DBItest::test_sql(skip = c(
 	"roundtrip_rownames", # this is a post 0.3.1 DBI feature
 	"roundtrip_date", # TODO: storing dates as DATE seems unsafe as there might be conflicts with the timezone. H2 to Java Date is -1h. Seems like H2 is storing the date at local UTC+1 and Date is UTC
 	"roundtrip_timestamp" # TODO: How can I differentiate between POSIXct and POSIXlt?
-), ctx = ctx)
+))
 DBItest::test_meta(skip = c(
 	"get_exception", # DBI 0.3.1 tells me to return a list not a character
 	"column_info", # DBI 0.3.1 tells me to return name, field.type, and data.type not "name" and "type"
 	"row_count", # TODO: fix. See comment in function
 	"bind($|_.+)" # dbBind is not defined in DBI 0.3.1
-), ctx = ctx)
+))
 DBItest::test_compliance(skip = c(
 	"read_only" # Why should writing to the database fail?
-), ctx = ctx)
+))
